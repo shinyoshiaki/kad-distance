6125e76c697ff6e974a9fae366370ccf3cef294f
shin-mi <nothing>
2018-08-21 23:51:04 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import * as util from "./util";
+
+export default function distance(a16, b16) {
+  let a = util
+    .convertBase(a16, 16, 2)
+    .toString()
+    .split("");
+  let b = util
+    .convertBase(b16, 16, 2)
+    .toString()
+    .split("");
+
+  let xor;
+  if (a.length > b.length) xor = new Array(a.length);
+  else xor = new Array(b.length);
+
+  for (let i = 0; i < xor.length; i++) {
+    xor[i] = parseInt(a[i], 10) ^ parseInt(b[i], 10);
+  }
+  let xored = xor.toString().replace(/,/g, "");
+  let n10 = parseInt(util.convertBase(xored, 2, 10).toString(), 10);
+
+  let n, i;
+  for (i = 0; ; i++) {
+    n = 2 ** i;
+    if (n > n10) break;
+  }
+  return i;
+}
---
+++ src/sample.js
@@ -0,0 +1,7 @@
+import distance from "./index";
+import sha1 from "sha1";
+
+const a = sha1(Math.random().toString()).toString();
+const b = sha1(Math.random().toString()).toString();
+
+console.log(distance(a, b));
